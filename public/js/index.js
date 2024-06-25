4f1abfac956b4be40741c75efc8d1f4dfe9512dd
SwrortechGitHub <benedikts.kuzmovs@gmail.com>
2024-06-25 18:40:09 +0300

    New design

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,24 @@
 /* Phone version burger icon*/
-const ToggleBotton = document.getElementById('ToggleBotton');
-const PagesLinks = document.getElementById('PagesLinks');
-ToggleBotton.addEventListener('click',()=> {
-    PagesLinks.classList.toggle('active');
+document.getElementById("Menu").addEventListener("click", function () {
+	document.getElementsByTagName("nav")[0].classList.toggle("active");
 });
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-    document.getElementsByTagName("nav")[0].style.top = "0";
-} else {
-    /*Hides navbar links when scrolling down*/
-    document.getElementById("PagesLinks").classList.remove("active")
-    document.getElementsByTagName("nav")[0].style.top = "-100px";
-}
-prevScrollpos = currentScrollPos;
-}
+/*Sidebar generator*/
+document.addEventListener("DOMContentLoaded", function () {
+	const ul = document.getElementById("Sidebar-list");
+	const h1Elements = document.querySelectorAll("main h1");
+
+	h1Elements.forEach((h1, index) => {
+		const li = document.createElement("li");
+		if (index === h1Elements.length - 1) {
+			li.className = "last";
+		}
+		li.textContent = h1.textContent;
+
+		const a = document.createElement("a");
+		a.href = `#${h1.id}`;
+		a.appendChild(li);
+
+		ul.appendChild(a);
+	});
+});
